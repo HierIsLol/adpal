c14e2fb118929de5100ee09d5b31a510216bee55
Damiën <da.hofle@gmail.com>
2024-08-03 17:50:04 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
+import { Auth } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+// Configureer Amplify met de outputs
 Amplify.configure(outputs);
+
+// Configureer de Auth module
+Auth.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
