70b755a46b8f13047d01f61e8529587f80cf2f16
Damiën <da.hofle@gmail.com>
2024-08-03 02:30:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { API, graphqlOperation } from 'aws-amplify';
 
 const client = generateClient<Schema>();
 
