69157b974ee0317c0c19b32e9fe85df0ad8c60f6
Damiën <da.hofle@gmail.com>
2024-08-03 14:46:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,9 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { useEffect, useState } from "react";
-import { listTodos } from './graphql/queries';
 import StoreLinkPage from './StoreLinkPage';
 
-type Todo = {
-  id: string;
-  content: string;
-  isDone: boolean;
-};
-
-type ListTodosResponse = {
-  data: {
-    listTodos: {
-      items: Todo[];
-      nextToken?: string;
-    };
-  };
-};
-
 function App() {
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://v6wglzzy3rgnfhgeks3ysp3ezq.appsync-api.us-east-1.amazonaws.com/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': 'da2-yan4cms26nbpjnzayiv5s3qrdm',
-        },
-        body: JSON.stringify({
-          query: listTodos,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const responseData: ListTodosResponse = await response.json();
-      const items = responseData.data.listTodos.items;
-      setTodos(items);
-    } catch (error) {
-      console.error("Error fetching todos", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <Router>
       <Authenticator>
