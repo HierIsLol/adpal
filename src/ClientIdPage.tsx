46022d4a77c97b5bf8e19e89d752a1e9a10421f9
Damiën <da.hofle@gmail.com>
2024-08-03 02:25:52 +0200

    Update ClientIdPage.tsx

--- src/ClientIdPage.tsx
+++ src/ClientIdPage.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 const ClientIdPage = () => {
     const [clientId, setClientId] = useState('');
     const [clientSecret, setClientSecret] = useState('');
