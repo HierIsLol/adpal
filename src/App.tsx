8b42a3e482d2cfaf27c34c2835d90772824f9e99
Damiën <da.hofle@gmail.com>
2024-08-03 12:01:59 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,49 @@
-import { API, graphqlOperation } from 'aws-amplify';
-import { listTodos } from './graphql/queries';
-import { useState, useEffect } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
-type Todo = {
-  id: string;
-  content: string;
-  isDone: boolean;
-};
+const client = generateClient<Schema>();
 
-type ListTodosResponse = {
-  data: {
-    listTodos: {
-      items: Todo[];
-      nextToken?: string;
-    };
-  };
-};
-
-const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await API.graphql(graphqlOperation(listTodos)) as ListTodosResponse;
-      const items = response.data.listTodos.items;
-      setTodos(items);
-    } catch (error) {
-      console.error("Error fetching todos", error);
-    }
-  };
+function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    fetchData();
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
   }, []);
 
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+
   return (
-    <div>
-      <h1>Todo List</h1>
+    <Authenticator>
+            {({ signOut, user }) => (
+      {({ signOut }) => (
+    <main>
+                <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.content} - {todo.isDone ? 'Done' : 'Not Done'}
-          </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-    </div>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+    <button onClick={signOut}>Sign out</button>
+    </main>
+      )}
+    </Authenticator>
   );
-};
+}
 
 export default App;
