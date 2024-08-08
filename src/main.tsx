900eab70eaa5b35a44b82debd6e1d93663047d67
Damiën <da.hofle@gmail.com>
2024-08-08 10:58:29 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import './app.css';
 
-// Configureer Amplify met de outputs
-Amplify.configure(outputs);
+import StoreLinkPage from './StoreLinkPage';
+import DashboardPage from './DashboardPage';
+import ProfilePage from './ProfilePage';
+import HomePage from './HomePage';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const AppContent: React.FC<{ signOut: () => void; user: any }> = ({ signOut, user }) => {
+  return (
+    <Router>
+      <div style={{ padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<HomePage user={user} signOut={signOut} />} />
+          <Route path="/store-link" element={<StoreLinkPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <AppContent signOut={signOut} user={user} />
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
