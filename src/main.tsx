58ffd6c839b8b8a98d03f7adf3b707f497ad3e5a
Damiën <da.hofle@gmail.com>
2024-08-08 11:24:53 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-// Bijgewerkte Amplify configuratie
-Amplify.configure({
-  region: 'us-east-1', // Verplaatst naar het hoofdniveau van de configuratie
-  Auth: {
-    userPoolId: 'us-east-1_hDRllzVfc',
-    userPoolWebClientId: '7n80mfbjksq1f05vu450ko4cs9',
-  }
-});
+// Configureer Amplify met de outputs
+Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      {({ signOut, user }) => (
-        <App signOut={() => signOut?.()} user={user} />
-      )}
-    </Authenticator>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
