a0a9cab533bff0baf9dac33ede0cdda4026b59da
Damiën <da.hofle@gmail.com>
2024-08-08 11:13:42 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
-Amplify.configure(awsExports);
+// Amplify configuratie met de juiste waarden
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1', // Dit is correct aangezien uw User Pool ID begint met 'us-east-1'
+    userPoolId: 'us-east-1_hDRllzVfc', // Uw User Pool ID
+    userPoolWebClientId: '7n80mfbjksq1f05vu450ko4cs9', // Uw App Client ID
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
