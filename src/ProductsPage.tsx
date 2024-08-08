7bf67d4835495e0369ccff6c498e2a4caa86f071
Damiën <da.hofle@gmail.com>
2024-08-08 14:35:28 +0200

    Create ProductsPage.tsx

---
+++ src/ProductsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductsPage: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '20px' }}>
+      <h1>Producten</h1>
+      <p>Hier kunt u uw producten bekijken en beheren.</p>
+      {/* Hier kunt u later meer functionaliteit toevoegen, zoals een lijst van producten of een formulier om productinformatie in te vullen */}
+    </div>
+  );
+};
+
+export default ProductsPage;
